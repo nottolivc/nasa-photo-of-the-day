9169329ec2b7990b62aefacc27962476555d303c
Andrew Nottoli <nottolivc@gmail.com>
2019-08-29 18:56:36 -0700

    added photocard to components

---
+++ components/PhotoCard.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const CustomImg = styled.img`
+padding: 20px;
+border-radius: 8px;
+&:hover {
+  opacity: 0.8;
+}
+`
+
+function PhotoCard(props) {
+
+    console.log(props);
+    return (
+        <div>
+        
+        <h1>NASA Photo of the Day</h1>
+            <div>
+            <h2>{props.title}</h2>
+
+            <img src={props.url} alt="NASA Photo of the Day" />
+
+            <p>{props.explanation}</p>
+            <p>{props.date}</p>git brach
+            </div>
+        </div>  
+    ); 
+}
+
+export default PhotoCard;
